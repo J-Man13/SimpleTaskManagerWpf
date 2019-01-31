ab1733cd34db50e92be3b7e883fc5fc8205fc145
JafarovJ <cafar.9r4@gmail.com>
2019-01-31 08:19:58 +0400

    first commit

---
+++ SimpleTaskManagerWpf/Views/TaskManagerMainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleTaskManagerWpf.Views
+{
+    /// <summary>
+    /// Interaction logic for TaskManagerMainWindow.xaml
+    /// </summary>
+    public partial class TaskManagerMainWindow : Window
+    {
+        public TaskManagerMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
